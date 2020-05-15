8c833a2de40330fd748f7014138bc02061aa7677
Armno <monkeyarmno@gmail.com>
2020-05-15 17:28:07 +0700

    sort tasks list by pinned tasks first

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -5,27 +5,51 @@ import { Task } from '../tasks/task';
   selector: 'app-task-list',
   template: `
     <div class="list-items">
-      <div *ngIf="loading">loading</div>
-      <div *ngIf="!loading && tasks.length === 0">empty</div>
       <app-task
-        *ngFor="let task of tasks"
+        *ngFor="let task of tasksInOrder"
         [task]="task"
         (onArchiveTask)="onArchiveTask.emit($event)"
         (onPinTask)="onPinTask.emit($event)"
       >
       </app-task>
+
+      <div
+        *ngIf="tasksInOrder.length === 0 && !loading"
+        class="wrapper-message"
+      >
+        <span class="icon-check"></span>
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
+
+      <div *ngIf="loading">
+        <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
+          <span class="glow-checkbox"></span>
+          <span class="glow-text">
+            <span>Loading</span> <span>cool</span> <span>state</span>
+          </span>
+        </div>
+      </div>
     </div>
   `,
   styleUrls: ['./task-list.component.less'],
 })
 export class TaskListComponent implements OnInit {
-  @Input() tasks: Task[] = [];
+  tasksInOrder: Task[] = [];
   @Input() loading = false;
 
   // tslint:disable-next-line: no-output-on-prefix
   @Output() onPinTask: EventEmitter<any> = new EventEmitter();
   // tslint:disable-next-line: no-output-on-prefix
   @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  @Input()
+  set tasks(tasks: Task[]) {
+    this.tasksInOrder = [
+      ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+      ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
+    ];
+  }
 
   constructor() {}
 
