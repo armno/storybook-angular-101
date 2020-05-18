62bdb9aa593a5642a296e13df09c60f681ec5392
Armno <monkeyarmno@gmail.com>
2020-05-18 12:00:43 +0700

    update task-list component specs

--- src/app/task-list/task-list.component.spec.ts
+++ src/app/task-list/task-list.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskListComponent } from './task-list.component';
+import { render } from '@testing-library/angular';
+import { TaskComponent } from '../tasks/task.component';
+import { withPinnedTasksData } from './task-list.stories';
 
 describe('TaskListComponent', () => {
-  let component: TaskListComponent;
-  let fixture: ComponentFixture<TaskListComponent>;
+  // let component: TaskListComponent;
+  // let fixture: ComponentFixture<TaskListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TaskListComponent ]
-    })
-    .compileComponents();
-  }));
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [TaskListComponent, TaskComponent],
+  //   }).compileComponents();
+  // }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(TaskListComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  it('renders pinned tasks at the start of the list', async () => {
+    const mockActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        tasks: withPinnedTasksData,
+        loading: false,
+        onPinTask: {
+          emit: mockActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockActions,
+        } as any,
+      },
+    });
+
+    const c = tree.fixture.componentInstance;
+    expect(c.tasksInOrder[0].id).toEqual('6');
   });
 });
